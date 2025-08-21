7d7d150c8d5b5ddb84842ba4ab3756b7ad309e58
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-08-21 20:59:51 -0300

    fix: Ajustes no MainContainer

--- src/components/main-container/main-container.tsx
+++ src/components/main-container/main-container.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const MainContainer = ({ children }: Props) => {
     return (
-        <main className="flex items-center justify-center h-full my-0 mx-auto w-full md:w-full  ">
+        <main className="flex items-center justify-center h-full my-0 mx-auto w-fit md:w-3xl md:max-w-5xl   lg:w-full lg:max-w-7xl">
             {children}
         </main>
     );
