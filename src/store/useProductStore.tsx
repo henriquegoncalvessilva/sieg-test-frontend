9219b1661c82d6fdac83f9994d0189b7678f6c78
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-08-22 15:10:14 -0300

    fix: Ajustes e mudança no nome da store para useProductStore

--- src/store/useProductStore.tsx
+++ src/store/useProductStore.tsx
@@ -1,16 +1,36 @@
 import { create } from "zustand";
+import type Produto from "../interfaces/card-item.interface";
 
 interface StoreState {
     isDrawerOpen: boolean;
     countCartItem: number;
+    inputSearch: string;
+    produtos: Produto[];
     toggleDrawer: () => void;
     incrementCountCartItem: () => void;
+    setInputValue: (value: string) => void;
     decrementCountCartItem: () => void;
+    setProductsData: (products: Produto[]) => void;
 }
 
-export const useStore = create<StoreState>((set) => ({
+export const useProductStore = create<StoreState>((set) => ({
     isDrawerOpen: false,
+    inputSearch: "",
     countCartItem: 0,
+    page: 1,
+    limit: 10,
+    produtos: [],
+    totalPages: 1,
+    setInputValue(value) {
+        set({
+            inputSearch: value,
+        });
+    },
+    setProductsData: (produtos) =>
+        set({
+            produtos,
+        }),
+
     toggleDrawer: () =>
         set((state) => ({
             isDrawerOpen: !state.isDrawerOpen,
