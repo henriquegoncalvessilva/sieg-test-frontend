89ebf33f0d72f32671bf2509a65491d81961f125
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-08-21 21:56:31 -0300

    fix: Ajustes no item card

--- src/components/card-Item/card-item.tsx
+++ src/components/card-Item/card-item.tsx
@@ -1,12 +1,20 @@
+import HeartIcon from "../../assets/icons/heart.svg";
+
 const CardItem = () => {
     return (
-        <div className="bg-[#fffffe] w-fit h-fit relative text-black">
-            <p className="absolute top-0 right-0 text-black">dsdd</p>
+        <div className="bg-[#fffffe] w-fit h-fit relative text-black p-2 pt-4">
+            <img
+                src={HeartIcon}
+                width={20}
+                height={20}
+                loading="lazy"
+                className="absolute  right-3 text-black"
+            />
             <img
                 loading="lazy"
                 src="https://cdn.dummyjson.com/product-images/beauty/essence-mascara-lash-princess/1.webp"
-                width={200}
-                height={200}
+                width={180}
+                height={180}
                 alt=""
             />
             <div className="flex flex-col gap-0 justify-between p-4">
