c64dded047d54ea25fe68e9c70cc4357973dd372
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-08-22 12:03:06 -0300

    feat: Implementação do React Memo para ajudar no desempenho da aplicação, principalmente quando for fazer a filtragem dos itens.

--- src/components/card-Item/card-item.tsx
+++ src/components/card-Item/card-item.tsx
@@ -1,6 +1,7 @@
 import HeartIcon from "../../assets/icons/heart.svg";
 import CartCard from "../../assets/icons/cart-card.svg";
 import { useStore } from "../../store/useStore";
+import React from "react";
 
 type CardItemProps = {
     id: number;
@@ -10,42 +11,44 @@ type CardItemProps = {
     preco: number;
 };
 
-const CardItem = ({ nome, preco, avaliacoes, image, id }: CardItemProps) => {
-    const addItemToCart = useStore((state) => state.incrementCountCartItem);
-    return (
-        <>
-            <div className="bg-[#fffffe] w-[196px] h-[308px] relative text-black p-2 pt-4">
-                <img
-                    src={HeartIcon}
-                    width={20}
-                    height={20}
-                    loading="lazy"
-                    className="absolute  right-3 text-black"
-                />
-                <img
-                    loading="lazy"
-                    src={image}
-                    width={180}
-                    height={180}
-                    alt=""
-                />
-                <div className="flex flex-col gap-0 justify-between p-4">
-                    <strong>{nome}</strong>
-                    <div className="flex"> Estrelas</div>
-                    <strong>{preco}</strong>
-                </div>
-                <div className="relative" onClick={addItemToCart}>
+const CardItem = React.memo(
+    ({ nome, preco, avaliacoes, image, id }: CardItemProps) => {
+        const addItemToCart = useStore((state) => state.incrementCountCartItem);
+        return (
+            <>
+                <div className="bg-[#fffffe] w-[196px] h-[308px] relative text-black p-2 pt-4">
                     <img
-                        src={CartCard}
+                        src={HeartIcon}
                         width={20}
                         height={20}
                         loading="lazy"
-                        className="absolute right-3 bottom-5"
+                        className="absolute  right-3 text-black"
                     />
+                    <img
+                        loading="lazy"
+                        src={image}
+                        width={180}
+                        height={180}
+                        alt=""
+                    />
+                    <div className="flex flex-col gap-0 justify-between p-4">
+                        <strong>{nome}</strong>
+                        <div className="flex"> Estrelas</div>
+                        <strong>{preco}</strong>
+                    </div>
+                    <div className="relative" onClick={addItemToCart}>
+                        <img
+                            src={CartCard}
+                            width={20}
+                            height={20}
+                            loading="lazy"
+                            className="absolute right-3 bottom-5"
+                        />
+                    </div>
                 </div>
-            </div>
-        </>
-    );
-};
+            </>
+        );
+    }
+);
 
 export default CardItem;
