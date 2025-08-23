52e5135855e7d971c57e3542a164fae8d6fc169d
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-08-23 16:45:58 -0300

    fix: Ajustes no filtro para deixar mais perfomático e sem precisar de novas requisições

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useProductStore } from "../../store/useProductStore";
+import type { Produto } from "../../interfaces/card-item.interface";
 
 type FiltersProps = {
     className?: string;
@@ -10,11 +11,58 @@ const Filters = ({ className }: FiltersProps) => {
     const [selectedPriceRange, setSelectedPriceRange] = useState<
         string | undefined
     >("");
+    const produtos = useProductStore((state) => state.produtos);
+    const setProdutos = useProductStore((state) => state.setProductsData);
+    const produtosOriginais = useRef<Produto[]>([]);
+
     const { clearFilters, inputSearch } = useProductStore();
 
     const handleClearFilters = () => {
+        setSelectedCategory("");
         clearFilters();
     };
+
+    useEffect(() => {
+        if (produtosOriginais.current.length === 0) {
+            produtosOriginais.current = produtos;
+            console.log(produtosOriginais.current);
+        }
+    }, [produtos]);
+
+    useEffect(() => {
+        if (selectedCategory.length > 0) {
+            setProdutos(
+                produtosOriginais.current.filter(
+                    (p) => p.category === selectedCategory
+                )
+            );
+        } else {
+            setProdutos(produtosOriginais.current);
+        }
+    }, [selectedCategory]);
+
+    const categories: string[] = [
+        "smartphones",
+        "laptops",
+        "fragrances",
+        "skincare",
+        "groceries",
+        "home-decoration",
+        "furniture",
+        "tops",
+        "womens-dresses",
+        "womens-shoes",
+        "mens-shirts",
+        "mens-shoes",
+        "mens-watches",
+        "womens-watches",
+        "womens-bags",
+        "womens-jewellery",
+        "sunglasses",
+        "automotive",
+        "motorcycle",
+        "lighting",
+    ];
 
     return (
         <section
@@ -25,7 +73,7 @@ const Filters = ({ className }: FiltersProps) => {
                     Filtros
                 </h2>
                 <button
-                    disabled={inputSearch == ""}
+                    disabled={inputSearch == "" && selectedCategory == ""}
                     type="button"
                     className="cursor-pointer disabled:text-gray-400 focus:border p-2"
                     onClick={handleClearFilters}
@@ -36,46 +84,29 @@ const Filters = ({ className }: FiltersProps) => {
             <div className="flex flex-col gap-4 w-full justify-between items-center">
                 <select
                     className="p-2 border rounded w-full  text-[#252427] text-xl"
-                    defaultValue={""}
                     onChange={(e) => setSelectedCategory(e.target.value)}
+                    value={selectedCategory}
                 >
                     <option className=" text-[#252427] text-xl" value="">
-                        Categorias
+                        Todos
                     </option>
-                    <option
-                        className=" text-[#252427] text-xl"
-                        value="smartphones"
-                    >
-                        Smartphones
-                    </option>
-                    <option className=" text-[#252427] text-xl" value="laptops">
-                        Laptops
-                    </option>
-                    <option
-                        className=" text-[#252427] text-xl"
-                        value="fragrances"
-                    >
-                        Fragrances
-                    </option>
-                    <option
-                        className=" text-[#252427] text-xl"
-                        value="skincare"
-                    >
-                        Skincare
-                    </option>
-                    <option
-                        className=" text-[#252427] text-xl"
-                        value="groceries"
-                    >
-                        Groceries
-                    </option>
+                    {categories.map((category) => (
+                        <option
+                            className=" text-[#252427] text-xl"
+                            key={category}
+                            value={category}
+                        >
+                            {category}
+                        </option>
+                    ))}
                 </select>
                 <div className="flex  xl:flex-col items-center gap-2 w-full ">
                     <label
                         htmlFor="price-range"
                         className="p-2 rounded w-full  text-[#252427] text-xl "
                     >
-                        Faixa de preço: R$ {selectedPriceRange}
+                        Faixa de preço: R${" "}
+                        {Number(selectedPriceRange).toFixed()}
                     </label>
                     <div className="flex w-full text-black items-center">
                         <p className="w-full text-center font-bold">R$ 0</p>
