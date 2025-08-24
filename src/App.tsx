5fca788c1515152bac64f9455458ea847ffb3533
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-08-24 17:43:30 -0300

    feat: Criação do componente Product Grid e importação do mesmo no componente principal.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import "./App.css";
 import Header from "./components/header/header";
 import MainContainer from "./components/main-container/main-container";
+import Filters from "./components/filters/filters";
+import { useResponsive } from "./hooks/useResponsive";
+import { useProductStore } from "./store/useProductStore";
+import Modal from "./components/modal/modal";
+import ScrollToTopButton from "./components/button-scroll-to-top/ScrollToTopButton";
+import useProducts from "./hooks/useProducts";
+import ProductGrid from "./components/product-grid/product-grid";
 
 function App() {
+    const isDesktop = useResponsive(1280);
+    const { produtos } = useProductStore();
+
+    useProducts({ search: "" });
+
     return (
-        <MainContainer>
-            <Header />
-        </MainContainer>
+        <>
+            <MainContainer>
+                <section className="w-full dark:bg-red-600">
+                    <Header />
+                    <Filters
+                        className={`${isDesktop ? "hidden" : "block w-full"}`}
+                    />
+                </section>
+
+                <ProductGrid produtos={produtos} />
+
+                <Modal />
+                <ScrollToTopButton />
+            </MainContainer>
+        </>
     );
 }
 
---
+++ src/components/product-grid/product-grid.tsx
@@ -0,0 +1,51 @@
+import { useResponsive } from "../../hooks/useResponsive";
+import type { Produto } from "../../interfaces/card-item.interface";
+import { useProductStore } from "../../store/useProductStore";
+import CardItem from "../card-Item/card-item";
+import CardItemSkeleton from "../card-Item/card-item-skeleton";
+import Filters from "../filters/filters";
+
+type ProductGridProps = {
+    produtos: Produto[];
+};
+
+const ProductGrid = ({ produtos }: ProductGridProps) => {
+    const loading = useProductStore((state) => state.loading);
+    const isDesktop = useResponsive(1280);
+
+    return (
+        <>
+            <section className="flex w-full justify-center gap-4 md:justify-start xl:justify-between xl:items-start relative">
+                <Filters
+                    className={`${
+                        isDesktop ? "block w-fit" : "hidden"
+                    } w-3xs items-start justify-start sticky top-6`}
+                />
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-fit m-auto">
+                    {loading
+                        ? Array.from({ length: 10 }).map((_, i) => (
+                              <CardItemSkeleton key={i + 1} />
+                          ))
+                        : produtos.map((p) => (
+                              <CardItem
+                                  className="cursor-pointer"
+                                  id={p.id}
+                                  key={p.id}
+                                  nome={p.title}
+                                  image={p.thumbnail}
+                                  avaliacoes={p.rating}
+                                  preco={p.price}
+                              />
+                          ))}
+                    {produtos.length === 0 && !loading && (
+                        <p className="col-span-full text-center text-gray-500">
+                            Nenhum produto encontrado.
+                        </p>
+                    )}
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default ProductGrid;
