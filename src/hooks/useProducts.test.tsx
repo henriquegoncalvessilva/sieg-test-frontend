b6197e76282edd1fbd864d7df55954c9c08b69bf
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-08-24 17:40:15 -0300

    feat: Criação do arquivo de teste para o hook Products.

---
+++ src/hooks/useProducts.test.tsx
@@ -0,0 +1,27 @@
+// products.test.tsx
+import { renderHook, waitFor } from "@testing-library/react";
+import * as api from "../services/api"; // onde estão suas funções fetchProductsByName, fetchAllProducts
+import useProducts from "./useProducts";
+
+jest.mock("../services/api");
+
+test("deve retornar produtos mockados", async () => {
+    const mockProducts = [
+        {
+            id: 1,
+            title: "Produto Teste",
+            price: 10.99,
+            category: "Vehicle",
+            rating: 4.5,
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+        },
+    ];
+
+    const mockedApi = api as jest.Mocked<typeof api>;
+    mockedApi.fetchAllProducts.mockResolvedValue(mockProducts);
+
+    const { result } = renderHook(() => useProducts({ search: "" }));
+    await waitFor(() => expect(result.current.data.length).toBeGreaterThan(0));
+    console.log(result.current.data);
+});
