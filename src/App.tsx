112d02839536e47647a92f7a4eb1bf36359009f8
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-08-25 11:52:29 -0300

    fix: remoção do filtro padrão e adição do filtro sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import "./App.css";
 import Header from "./components/header/header";
 import MainContainer from "./components/main-container/main-container";
-import Filters from "./components/filters/filters";
-import { useResponsive } from "./hooks/useResponsive";
+
 import { useProductStore } from "./store/useProductStore";
 import Modal from "./components/modal/modal";
 import ScrollToTopButton from "./components/button-scroll-to-top/ScrollToTopButton";
 import ProductGrid from "./components/product-grid/product-grid";
+import FilterSideBar from "./components/filters/filters-sidebar";
 
 function App() {
-    const isDesktop = useResponsive(1280);
     const { produtos } = useProductStore();
 
     return (
@@ -17,13 +16,11 @@ function App() {
             <MainContainer>
                 <section className="w-full">
                     <Header />
-                    <Filters
-                        className={`${isDesktop ? "hidden" : "block w-full"}`}
-                    />
                 </section>
 
                 <ProductGrid produtos={produtos} />
 
+                <FilterSideBar />
                 <Modal />
                 <ScrollToTopButton />
             </MainContainer>
