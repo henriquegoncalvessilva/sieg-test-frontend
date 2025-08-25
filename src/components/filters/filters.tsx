364af815b40de4f8ce6076288f939b6c956b00fb
Biro <luizhenriquegoncalvessilva@hotmail.com>
2025-08-25 11:07:20 -0300

    feat: Adição do sort by

--- src/components/filters/filters.tsx
+++ src/components/filters/filters.tsx
@@ -23,12 +23,17 @@ const Filters = ({ className }: FiltersProps) => {
     const { clearFilters } = useProductStore();
     const sortValue = useProductStore((state) => state.sortValue);
     const valueSearch = useProductStore((state) => state.inputSearch);
+    const setSort = useProductStore((state) => state.setSort);
 
     const handleClearFilters = () => {
         setSelectedCategory("");
         setBadgeFilters([]);
         setSelectedPriceRange(undefined);
         clearFilters();
+    };
+
+    const handleSortChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSort(e.target.value);
     };
 
     useEffect(() => {
@@ -113,18 +118,39 @@ const Filters = ({ className }: FiltersProps) => {
                 Total Products: {products.length}
             </small>
             <div className="flex gap-4 justify-between items-center w-full">
-                <div className="flex gap-2 items-center justify-center">
-                    <h2 className="font-bold text-left text-[#252427] text-xl">
-                        Filters
-                    </h2>
-                    <p className="px-3 bg-black text-white rounded-md self-end">
-                        {badgeFilters.length}
-                    </p>
+                <div className="flex gap-8 items-start justify-start flex-col">
+                    <div className="flex gap-2 items-start justify-start">
+                        <h2 className="font-bold text-left text-[#252427] text-xl">
+                            Filters
+                        </h2>
+                        <p className="px-3 bg-black text-white rounded-md self-end">
+                            {badgeFilters.length}
+                        </p>
+                    </div>
+                    <div className="flex items-center self-start mb-5 gap-1">
+                        <p className="text-[#252427] text-sm md:text-xl">
+                            Sort by:{" "}
+                        </p>
+                        <form className="self-start w-fit bg-white p-2 rounded-md">
+                            <select
+                                name="sort"
+                                value={sortValue}
+                                id="sort"
+                                onChange={handleSortChange}
+                            >
+                                <option value="">Default</option>
+                                <option value="asc">A - Z</option>
+                                <option value="desc">Z - A</option>
+                                <option value="expensive">Expensive</option>
+                                <option value="cheap">Cheap</option>
+                            </select>
+                        </form>
+                    </div>
                 </div>
 
                 <Button
                     disabled={badgeFilters.length === 0}
-                    className="cursor-pointer p-2  hover:text-white "
+                    className="cursor-pointer p-2  hover:text-white self-start "
                     onClick={handleClearFilters}
                 >
                     Clear Filters
